0de76184467612671274d5c2af0063190b73c53a
xor26 <sc1.upsilon@gmail.com>
2020-01-29 20:15:45 +0300

    Added consumer

--- configs/config.py
+++ configs/config.py
@@ -9,3 +9,8 @@ class FlaskConfig:
 class KafkaConfig:
     KAFKA_HOST_NAME = os.environ.get("KAFKA_HOST_NAME") or "localhost"
     KAFKA_HOST_PORT = os.environ.get("KAFKA_HOST_PORT") or "9092"
+
+
+class RedisConfig:
+    KAFKA_HOST_NAME = os.environ.get("REDIS_HOST_NAME") or "localhost"
+    KAFKA_HOST_PORT = os.environ.get("REDIS_HOST_PORT") or "6379"
--- kafka_consumer/kafka_consumer.py
+++ kafka_consumer/kafka_consumer.py
@@ -1,18 +1,26 @@
 from kafka import KafkaConsumer
 from json import loads
+import redis
+from configs.config import RedisConfig
+from configs.config import KafkaConfig
+
 
 consumer = KafkaConsumer(
     "files",
-    bootstrap_servers=["kafka:9092"],
+    bootstrap_servers=[f"{KafkaConfig.KAFKA_HOST_NAME}:{KafkaConfig.KAFKA_HOST_PORT}"],
     auto_offset_reset="earliest",
     enable_auto_commit=True,
     group_id="my-group",
     value_deserializer=lambda x: loads(x.decode("utf-8")))
 
+db = redis.Redis(host=RedisConfig.KAFKA_HOST_NAME, port=RedisConfig.KAFKA_HOST_PORT)
+
 print("Starting...")
 
 for message in consumer:
     message = message.value
-    print("Received: " + str(message))
+    db.mset({message["file_name"]: message["file_path"]})
 
-print(1)
+    print("Received: " + str(message))
+    print("Stored: " + str(db.get(message["file_name"])
+                           ))
